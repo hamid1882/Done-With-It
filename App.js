ffdaa3ea45606cab94a266a513182a741e65a44f
hamid <hussainhafeez1100@gmail.com>
2022-07-09 18:39:52 +0530

    Edyoda Journey

--- App.js
+++ App.js
@@ -4,50 +4,36 @@ import {
   StyleSheet, 
   Text, 
   View, 
-  SafeAreaView, 
-  Image,
-  TouchableNativeFeedback,
-  Button,
-  Alert,
+  SafeAreaView,
   Platform,
-  StatusBar
+  StatusBar,
+  Image,
+  TextInput,
+  Button
   } from 'react-native';
+import Topbar from './Components/Topbar';
 
 export default function App() {
-
-  const dimensions = useDimensions("screen");
-
-  const orientation = useDeviceOrientation();
-
-  const handlePress = () => {
-    console.log("Hamid you pressed")
-  }
-
   return (
     <SafeAreaView style={styles.container}>
-      <Text numberOfLines={1} onPress={handlePress}>Hello React native- Journey! - Alhamdulillah finnaly you are learning something</Text>
-      <Image
-        blurRadius={10}
-        fadeDuration={1000}
-        source={{
-            width: 200,
-            height: 300,
-            uri: "https://picsum.photos/200/300"
-         }}/>
-         <TouchableNativeFeedback>
-          <View style={{ width: 200, height: 100, backgroundColor:"skyblue"}}></View>
-         </TouchableNativeFeedback>
-         <Button
-            title="Hamid here!"
-            color="black"
-            onPress={() => Alert.alert("Hamidoo","Hamid your alert",[
-              {
-                text: "Hello",
-                style: { backgroundColor: "red", color: "red"}
-              }
-            ])}
-         />
-      <StatusBar style="auto" />
+      <Topbar style={styles} />
+      <View style={styles.WelcomeBar}>
+        <Text style={styles.Text100}>Welcome to Edyoda!</Text>
+        <Text style={styles.Text100}>Login to Continue Learning...</Text>
+      </View>
+      <View style={styles.LoginBar}>
+        <TextInput style={{
+            borderBottomWidth: 1,
+            borderBottomColor: "#ccc",
+           }} 
+           placeholder='Username/Email' />
+           <TextInput style={{
+            borderBottomWidth: 1,
+            borderBottomColor: "#ccc",
+           }} 
+           placeholder='Password' />
+        <Button title='Login'/>
+      </View>
     </SafeAreaView>
   );
 }
@@ -55,7 +41,46 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: "#efefef",
     paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
   },
+  Topbar: {
+    width: "100%",
+    flex: 0.1,
+    padding: 10,
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    flexDirection: "row"
+  },
+  Logo: {
+    color: "#0084ff",
+    fontSize: 30,
+    fontFamily: "roboto",
+    fontWeight: "bold"
+  },
+  MoreIcon: {
+    width: 40,
+    height: 40,
+  },
+  WelcomeBar: {
+    flex: 0.1,
+    justifyContent: "center",
+    alignItems: "center",
+    textAlign: "center",
+  },
+  Text100: {
+    fontFamily: "sans-serif",
+    fontSize: 20,
+    letterSpacing: 0.5,
+  },
+  LoginBar: {
+    flex: 0.5,
+    justifyContent: "center",
+    backgroundColor: "#fff",
+    margin: 15,
+    marginRight: 30,
+    marginLeft: 30,
+    paddingLeft: 50,
+    paddingRight: 50,
+  }
 });
